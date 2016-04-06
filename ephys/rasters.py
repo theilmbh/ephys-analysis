3bfe429e9eb19662875cc58f113f8dd75fcddad8
Brad Theilman <btheilma@ucsd.edu>
2016-04-06 16:26:58 -0700

    Single cell plotting

--- ephys/rasters.py
+++ ephys/rasters.py
@@ -42,5 +42,24 @@ def do_raster(raster_data, times, ticks, ax=None):
 
 
 
-def plot_raster_single_cell(block_path, clusterID):
-	print('Nothing')
+def plot_raster_cell_stim(block_path, clusterID, stim, period):
+
+	spikes = core.load_spikes(block_path)
+	trials = events.load_trials(block_path)
+	fs = core.load_fs(block_path)
+	
+	stim_trials = trials[trials['stimulus']==stim]
+	ntrials = len(stim_trials)
+	stim_starts = stim_trials['time_samples'].values
+	stim_ends = stim_trials['stimulus_end'].values
+
+	stim_end_seconds = np.unique((stim_ends - stim_starts)/fs)[0]
+	window = [period[0], stim_end_seonds[trial]+period[1]]
+	raster_data = []
+	for trial, start in enumerate(stim_starts):
+		
+		sptrain = get_spiketrain(rec, start, clusterID, spikes, window)
+		raster_data.append(sptrain)
+
+	do_raster(raster_data, window, [0, stim_end_seconds])
+
