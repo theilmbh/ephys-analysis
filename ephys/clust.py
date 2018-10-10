b7b8275181c7b15f86432e77fb5cb8bd90398d34
Marvin T <MarvinT@users.noreply.github.com>
2018-10-10 15:12:45 -0700

    get width half height (#44)
    * added get_width_half_height to clust
    
    * edited clust to get width at half height

--- ephys/clust.py
+++ ephys/clust.py
@@ -194,6 +194,48 @@ def get_troughpeak(time, spike_shape):
     return time[trough_i], time[peak_i]
 
 
+def get_width_half_height(time,spike_shape):
+    '''
+    grabs the time between the zero crossings around trough after normalize to min height and offset by 0.5
+    
+    Parameters
+    ------
+    time : numpy array
+    spike_shape : numpy array %should be up-sampled to at least 100000 before calculating
+    
+    Returns
+    ------
+    width_half_height : float
+        time between crossings in seconds
+
+    '''
+    width = np.nan;
+    
+    trough,peak = get_troughpeak(time,spike_shape)
+    ind = np.where(time == trough)
+    troughind = ind[0][0]
+    
+    spike_shape /= -spike_shape.min()
+    spike_shape = spike_shape + 0.5
+    zero_crossings = np.where(np.diff(np.sign(spike_shape)))[0]
+    
+    i = zero_crossings < troughind
+    j = zero_crossings > troughind
+
+    if (True in i) & (True in j):
+        # zero crossings before trough
+        i = (np.where(i))
+        pre_ind = zero_crossings[max(i[0])]
+        
+        # zero crossings after trough
+        j = (np.where(j))
+        post_ind = zero_crossings[min(j[0])]
+        
+        width = time[post_ind] - time[pre_ind]
+
+    return width
+
+
 def get_width(block_path, cluster, new_fs=1000000.0):
     '''
     grabs the time of the trough and peak
